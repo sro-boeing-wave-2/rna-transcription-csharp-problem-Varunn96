5e59bf3defe18a9df092136837e22c5a512cc4ff
Varun Naidu <n.varunnaidu@gmail.com>
2018-07-19 16:49:45 +0530

    added code

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace RNATranscriptionModule
 {
     public static class RnaTranscription
     {
         public static string ToRna(string nucleotide)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            Dictionary<char, char> dnaToRna = new Dictionary<char, char>();
+            dnaToRna.Add('G', 'C');
+            dnaToRna.Add('C', 'G');
+            dnaToRna.Add('T', 'A');
+            dnaToRna.Add('A', 'U');
+
+            char[] dnaArray = nucleotide.ToCharArray();
+            char[] rnaArray = new char[dnaArray.Length];
+            for (int i = 0; i < dnaArray.Length; i += 1)
+            {
+                rnaArray[i] = dnaToRna[dnaArray[i]];
+            }
+            return String.Concat(rnaArray);
+            //throw new NotImplementedException("You need to implement this function.");
         }
     }
 }
